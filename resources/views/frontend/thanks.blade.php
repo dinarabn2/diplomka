903c08a076c4fcd0ebc5eb809631a359adca8c5e
dinarabn2 <107791872+dinarabn2@users.noreply.github.com>
2023-05-01 05:31:35 +0600

    test

--- resources/views/frontend/thanks.blade.php
+++ resources/views/frontend/thanks.blade.php
@@ -1,10 +1,25 @@
 @extends('layouts.front')
 
 @section('content')
+<style>
+    .thanks-bg {
+        display: flex;
+        align-items: center;
+        height: 100vh;
+        justify-content: center;
+    }
+    .alert {
+        width: 80%;
+    }
+</style>
+
 <div class="container">
-    <div class="alert alert-success" role="alert">
-        <h1>Студент сәтті сақталды.</h1>
-        <a href="/" class="btn btn-primary">Home</a>
+   <div class="thanks-bg">
+    <div class="alert alert-success tac" role="alert">
+        <h1>Өтінішіңіз сәтті жіберілді.</h1>
+        <h2>Жауапты почта арқылы күтіңіз.</h2>
+        <a href="/" class="btn btn-primary mt-5">Басты бетке</a>
     </div>
+   </div>
 </div>
 @endsection
