76bbeb9a2e5988186d4ebfe2ca93c8312f5e9432
Arman <arman.yu@evrika.com>
2023-05-02 09:44:43 +0600

    prublic/files gitignored, mail file begin

---
+++ app/Mail/SendNotification.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Student;
+
+class SendNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $student;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Student $student)
+    {
+        $this->student = $student;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('univer@edu.kz', 'Notificatiob')
+            ->view('emails.notification')
+            ->with([
+                'name' => $this->student->name,
+                'surname' => $this->student->surname,
+                'email' => $this->student->email
+            ]);
+    }
+}
---
+++ resources/views/emails/notification.blade.php
@@ -0,0 +1,5 @@
+<div>
+    Student name: {{ $name }}
+    Student surname: {{ $surname }}
+    Student email: {{ $email }}
+</div>
